9c37ec90870192a26c45079a78326baaa3f51b8b
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2025-09-07 18:48:24 +0200

    Header aangemaakt

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curio Opendag</title>
-
 </head>
 
 <body>
     <header>
-
+        <div class="header-container">
+                  <!-- Todo afbeelding -->
+            <nav>
+                <a href="#intro">Introductie</a>
+                <a href="#vakken">Vakken</a>
+                <a href="#rooster">Lesrooster</a>
+                <a href="#trajecten">Trajecten</a>
+                <a href="#eigen-mening">Eigen mening</a>
+            </nav>
+        </div>
     </header>
     <main>
 
