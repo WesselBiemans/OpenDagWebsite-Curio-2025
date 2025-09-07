435b7f7c954da1334d3929cf98e340f1bcbcf1a8
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2025-09-07 18:55:01 +0200

    Vakken toegevoegd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,9 +31,79 @@
                     <input type="submit" id="sign-up" value="Meld je aan!" />
                 </form>
             </div>
-            
+            <p id="vakken"></p>
         </div>
 
+        <div class="vakken-container">
+            <div class="container">
+                <h2>Vakken</h2>
+                <div class="vakken">
+                    <div class="vak">
+                        <h3>Ne</h3>
+                        <div class="vak-description" id="ne" hidden>
+                            <p>Nederlands is een taalvak die je voor deze opleiding moet behalen. Je krijgt onderwerpen zoals samenhang, inleiding, middel en slot en tekstsoorten.</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>En</h3>
+                        <div class="vak-description" id="en" hidden>
+                            <p>Engels is een taalvak die je voor deze opleiding moet behalen. Bij elk hoofdstuk leer je hoe je het onderwerp van gesprekken of verhalen begrijpt in het engels.</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>Re</h3>
+                        <div class="vak-description" id="rek" hidden>
+                            <p>Rekenen is een vak waar je met sommen werkt. Dit vak kan je in het eerste blok al afronden als je een goed resultaat behaalt voor je oefenexamen en daarna ervoor kiest om je eindexamen te maken.</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>WEB</h3>
+                        <div class="vak-description" id="web" hidden>
+                            <p>Bij dit vak leer je hoe je met codeer talen zoals HTML, CSS en Javascript een website kan maken, zoals deze! Ook leer je werken met databases en het CRUD-systeem (Create, Read, Update, Delete).</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>NATIVE</h3>
+                        <div class="vak-description" id="native" hidden>
+                            <p>Met NATIVE leer je vormen van programmeren die niet worden geleerd bij WEB, zoals het maken van scripts en Applicaties. Voor het eerste blok gebruik je de codeertaal Python, waarna je in volgende blokken overstapt naar C#.</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>PRA</h3>
+                        <div class="vak-description" id="pra" hidden>
+                            <p>PRA staat voor Practijkwerk. Hierbij gaat het over het leren werken in een groep en werken met een opdrachtgever. Ook leer je een manier van werken genaamd AGILE via de SCRUM methode.</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>PRO</h3>
+                        <div class="vak-description" id="pro" hidden>
+                            <p>Bij PRO doe je geen programmeren, maar leer je de stappen voor het maken van een project. Denk aan het plannen en stappenplan, maar ook reflecteren en verbeteren naderhand.</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>Mtg</h3>
+                        <div class="vak-description" id="mtg" hidden>
+                            <p>Dit is een geplande moment waar de student met de mentor 1 op 1 samen zitten om te bespreken, bijvoorbeeld over prestaties, versnellen of potentiele frustraties.</p>
+                        </div>
+                    </div>
+
+                    <div class="vak">
+                        <h3>Mtu</h3>
+                        <div class="vak-description" id="mtu" hidden>
+                            <p>Elke week is er een Mentor uur, waarin de mentor met de klas over bepaalde school-gerelateerde onderwerpen praat, zoals opkomende toetsweken of om algemene vragen beantwoordt.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
     </main>
 
@@ -41,7 +111,6 @@
 
     </footer>
 </body>
-
 
 
 </html>
