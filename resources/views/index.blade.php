8469d392efcbf2e7483832478ea8227f98889742
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2025-09-07 18:52:41 +0200

    Alle afbeeldingen + Header logo + intro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curio Opendag</title>
+
 </head>
 
 <body>
     <header>
         <div class="header-container">
-                  <!-- Todo afbeelding -->
+            <img src="{{ asset('img/curio-logo.png') }}" alt="Curio Logo">
             <nav>
                 <a href="#intro">Introductie</a>
                 <a href="#vakken">Vakken</a>
@@ -21,6 +22,18 @@
         </div>
     </header>
     <main>
+        <p id="intro"></p>
+        <div class="intro-container">
+            <div class="container">
+                <h1>Hello World!</h1>
+                <p>Software is tegenwoordig in allerlei apparaten, bijvoorbeeld TV's, telefoons, computers, koelkasten en auto's om er maar een paar te noemen. Het is een onmisbare onderdeel in onze huidige samenleving en helpt miljoenen mensen elke dag, dus is het van belang dat er software developers zijn die dat kan ontwikkelen. Dat kan je bij deze opleiding leren!</p>
+                <form action="https://www.curio.nl/mbo/ict/ict/software-developer" target="_blank">
+                    <input type="submit" id="sign-up" value="Meld je aan!" />
+                </form>
+            </div>
+            
+        </div>
+
 
     </main>
 
